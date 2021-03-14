79ab34199916eaef5f67458463066bcfeb10c698
Giang Nguyen <giangkawaii1999@gmail.com>
2021-03-14 15:14:27 -0400

    set up react client

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    // Switch,
+    // Route,
+    // Link,
+    // Redirect,
+} from 'react-router-dom';
+//
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [isLogin, setIsLogin] = useState(false);
+    return (
+        <Router>
+            <Navbar bg='light' expand='lg'>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    {isLogin ? (
+                        <Nav className='mr-auto'>
+                            <Nav.Link href='/home'>Home</Nav.Link>
+                            <Nav.Link href='/list'>List of Users</Nav.Link>
+                            <Nav.Link href='/listarticles'>
+                                List of Articles
+                            </Nav.Link>
+                        </Nav>
+                    ) : (
+                        <Nav className='mr-auto'>
+                            <Nav.Link href='/home'>Home</Nav.Link>
+                            <Nav.Link href='/login'>Login</Nav.Link>
+                            <Nav.Link href='/list'>List of Users</Nav.Link>
+                            <Nav.Link href='/listarticles'>
+                                List of Articles
+                            </Nav.Link>
+                            <Nav.Link href='/create'>Sign Up</Nav.Link>
+                        </Nav>
+                    )}
+                </Navbar.Collapse>
+            </Navbar>
 
+            <div className='aws'>
+                <Button variant='danger' className='btn-lg'>
+                    Home
+                </Button>
+                <h1>hello world</h1>
+            </div>
+        </Router>
+    );
+}
 export default App;
